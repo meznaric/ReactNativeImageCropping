757014e4ecb7987a8abba6f62b51461c4b56f9fe
Murad R <37542484+mraucorp@users.noreply.github.com>
2018-04-09 18:05:32 +0300

    fix "Error: Redefinition of RCTMethodInfo"
    following https://github.com/facebook/react-native/issues/15775
    and this PR - https://github.com/nico1510/react-native-image-crop-picker/pull/9/files
    
    it works for me this way
    i am not sure about the right order for TOCropView.h, just kept it between

--- ReactNativeImageCropping.h
+++ ReactNativeImageCropping.h
@@ -1,6 +1,12 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#import "TOCropView.h"
+#import <React/RCTImageLoader.h>
+#else
 #import "RCTBridgeModule.h"
 #import "TOCropView.h"
-#import <RCTImageLoader.h>
+#import "RCTImageLoader.h"
+#endif
 
 @interface ReactNativeImageCropping : NSObject <RCTBridgeModule>
 
